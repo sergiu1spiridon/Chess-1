d55edf6f07fe12d4b7449738935f897f79cc2322
sergiu1spiridon <srg.mihai200059@gmail.com>
2020-08-06 22:58:52 +0300

    Changed Hashtable.c (complete?)

--- Hashtable.c
+++ Hashtable.c
@@ -1,8 +1,8 @@
 #include "Hashtable.h"
+#include "LinkedList.h"
 
 unsigned long
-hash(unsigned char *str)
-{
+hash(unsigned char *str) {
     unsigned long hash = 5381;
     int c;
 
@@ -10,4 +10,42 @@ hash(unsigned char *str)
         hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
     return hash;
+}
+
+void addToHash(hashtable *hash,unsigned char *key, void *info) {
+	unsigned long indexOfElement = hash->hashFunction(key) % hash->size;
+
+	push(hash->bucket[indexOfElement], info);
+}
+
+void *getFromHash(hashtable *hash, unsigned char *key) {
+	unsigned long indexOfElement = hash->hashFunction(key) % hash->size;
+
+	return pop(hash[indexOfElement]);
+}
+
+hashtable initHashtable(int size) {
+	hashtable *hash = malloc(sizeof(hashtable));
+
+	if (!hash)
+	{
+		printf("Hash not formed correctly\n");
+		return NULL;
+	}
+
+	hash->bucket = malloc(sizeof(List *) * size);
+
+	if(hash->bucket == NULL) {
+		printf("Buckets not formed correctly\n");
+		return NULL;
+	}
+
+	for (int i = 0; i < size; i++) {
+		(hash->bucket + i) = (hash->bucket + i - 1) + sizeof(List*);
+
+		hash->bucket[i]->head = NULL;
+		hash->bucket[i]->size = 0;
+	}
+
+	return hash;
 }
