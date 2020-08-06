0d682f16653c4d1c7a25a2e2d3171e8358e728ed
sergiu1spiridon <srg.mihai200059@gmail.com>
2020-08-06 21:09:24 +0300

    mesage2

--- Hashtable.h
+++ Hashtable.h
@@ -3,7 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct hashtable
-{
-	
+typedef struct buckets {
+	int numberOfTimesInCheck;
+}buckets;
+
+typedef struct hashtable {
+	buckets *bucket;	
 }hashtable;
