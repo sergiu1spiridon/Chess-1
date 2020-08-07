4f88db5d038d731499d7c80514fc86cb54b3580e
tavis <mateitavis@gmail.com>
2020-08-07 18:12:10 +0300

    Update Game_Logic.h

--- Game_Logic.h
+++ Game_Logic.h
@@ -3,4 +3,14 @@
 
 #include "common.h"
 
+// get the key for the hashtable from the current state
+unsigned char* getKeyFromChessTable(unsigned char**chessMatrix);
+// get the current state from the hashtable
+unsigned char** getChessTableFromKey(unsigned char* key);
+// get the score of the state from the table
+int getScoreFromChessTable(unsigned char** chessMatrix);
+// get the best piece for the next move
+unsigned char* getPieceFromChessTable(unsigned char** chessMatrix);
+
+
 #endif/*GAME_LOGIC*/
