11b218799c4168faaf1d0d33e68a272aac00fbc5
tavis <mateitavis@gmail.com>
2020-08-07 20:26:38 +0300

    Game_Logic

--- Game_Logic.h
+++ Game_Logic.h
@@ -7,7 +7,44 @@
 #include <string.h>
 
 #include "HashTable.h"
+/*
+* Score value is calculated by the formula:
+* Sum of current pieces +
+* 
+* 
+* Chess encoding:
+* P - Pawn
+* k - knight
+* B - bishop
+* R - rook
+* Q - queen
+* K - king
+* 
+* 
+*/
+char pieceValue[7][8]={ "PEGMGEG",
+                        "P010101",
+                        "k030405",
+                        "B030405",
+                        "R050607",
+                        "Q091013",
+                        "K000101"};
+/*
+ * Initial chess matrix
+*/
+char initialChessMatrix[8][8]={   "RkBQKBkR",
+                                    "PPPPPPPP",
+                                    "********",
+                                    "********",
+                                    "********",
+                                    "********",
+                                    "PPPPPPPP",
+                                    "RkBQKBkR"};
 
+// get the difference between the current and initial states
+int diffStates(unsigned char**currentMatrix);
+// get the total number of pieces in game
+int countPieces(unsigned char**currentMatrix);
 // get the key for the hashtable from the current state
 unsigned char* getKeyFromChessTable(unsigned char**chessMatrix);
 // get the current state from the hashtable
--- Game_logic.c
+++ Game_logic.c
@@ -1,1 +1,96 @@
 #include "Game_Logic.h"
+
+// get the difference between the current and initial states
+int diffStates(unsigned char**currentMatrix)
+{
+    // the difference between the two Matrices
+    int diffCount = 0;
+    
+    for(int i= 0;i<8;i++)
+    {
+        for(int j=0;j<8;j++)
+        {
+            if((int)currentMatrix[i][j]!=(int)initialChessMatrix[i][j]) diffCount++;
+        }
+    }
+
+    return diffCount;
+}
+// get the total number of pieces in game
+int countPieces(unsigned char**currentMatrix)
+{
+    // the difference between the two Matrices
+    int count = 0;
+    
+    for(int i= 0;i<8;i++)
+    {
+        for(int j=0;j<8;j++)
+        {
+            if(currentMatrix[i][j]!='*') count++;
+        }
+    }
+
+    return count;
+}
+// get the key for the hashtable from the current state
+unsigned char* getKeyFromChessTable(unsigned char**chessMatrix)
+{
+    unsigned char* local_key = malloc(sizeof(unsigned char)*192);
+
+    for(unsigned int i = 0;i<8;i++)
+    {
+        for(unsigned int j=0;j<8;j++)
+        {
+            strcat(local_key,(char*)&chessMatrix[i][j]);
+            strcat(local_key,(char*)&i);
+            strcat(local_key,(char*)&j);
+
+        }
+    }
+    printf("%s\n",local_key);
+    return local_key;
+}
+// get the current state from the hashtable
+unsigned char** getChessTableFromKey(unsigned char* key)
+{
+    unsigned char ** chessMatrix = malloc(sizeof(unsigned char*)*8);
+    
+    for(int i= 0;i<8;i++)
+    {
+        chessMatrix[i] = malloc(sizeof(unsigned char)*8);
+        for(int j=0;j<24;j+=3)
+        {
+            strcpy((char*)&chessMatrix[j+1][j+2],(char*)&j);
+        }
+    }
+    return chessMatrix;
+}
+// get the score of the state from the table
+int getScoreFromChessTable(unsigned char* chessMatrixKey)
+{
+    return 0;
+}
+// get the best piece for the next move
+unsigned char* getPieceFromChessTable(unsigned char** chessMatrix)
+{
+    int diffCount = diffStates(chessMatrix);
+    int totalPieces = countPieces(chessMatrix);
+
+    // early game is defined as diffCount < 16
+    // middle game is defined as diffCount >= 16 and total pieces >=8
+    // end game is otherwise
+
+    if(diffCount < 16)
+    {
+        //early game
+    }
+    else if(diffCount >=16 && totalPieces >=8)
+    {
+        //middle game
+    }
+    else
+    {
+        //end game
+    }
+    
+}
