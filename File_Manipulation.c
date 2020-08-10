79c77c2cf74d3753ab0364c81d1a87bb65e83113
tavis <mateitavis@gmail.com>
2020-08-10 14:07:27 +0300

    Change in File_Manipulation

---
+++ File_Manipulation.c
@@ -0,0 +1,61 @@
+#include "File_Manipulation.h"
+
+void readFromFile(hashtable *hash) {
+    FILE *file;
+    file = fopen("myFile", "r");
+
+    if (file == NULL)
+    {
+        printf("File not found");
+        return;
+    }
+
+    if(!hash) {
+		printf("Hash nonexistent\n");
+		return;
+	}
+    unsigned char *parent, *child; 
+    while ((int)fscanf(file, "%s %s ", parent, child) != 0)
+    {
+        addChildToParent(hash, child, parent);
+    }
+    
+    fclose;
+}
+
+void writeToFile(hashtable *hash) {
+    FILE *file;
+    file = fopen("myFile", "w");
+
+    if (file == NULL)
+    {
+        printf("File not found");
+        return;
+    }
+
+    if(!hash) {
+		printf("Hash nonexistent\n");
+		return;
+	}
+
+	Node *currentNode;
+	for (int i = 0; i < hash->size; ++i)
+	{	
+		currentNode = hash->bucket[i]->head;
+		while (currentNode)
+		{
+			for (int i = 0; i < currentNode->info->numberOfParents; i++)
+			{
+				fprintf(file, "%s %s\n", currentNode->info->parents[i], currentNode->info->key);
+			}
+
+			for (int i = 0; i < currentNode->info->heap->size; i++)
+			{
+				fprintf(file, "%s %s\n",currentNode->info->key, currentNode->info->heap->myHeap[i]->key);
+			}
+			currentNode = currentNode->next;
+		}
+	}
+
+    fclose(file);
+}
--- File_Manipulation.h
+++ File_Manipulation.h
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "Hashtable.h"
+#include "Game_Logic.h"
+
 void readFromFile(hashtable *hash);
 
 void writeToFile(unsigned char *parentKey, unsigned char *childKey);
