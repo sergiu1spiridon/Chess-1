8e26ddea82295a714d27977f1710e5907013891e
tavis <mateitavis@gmail.com>
2020-08-10 23:01:25 +0300

    Update Check_Mate.c

--- Check_Mate.c
+++ Check_Mate.c
@@ -1,7 +1,219 @@
 #include "Check_Mate.h"
 
-unsigned char **movePawn(unsigned char **matrix, pieceCoordonate *myPiece) {
+bool movePawn(unsigned char **matrix, pieceCoordonate *myPiece) {
     unsigned char **newMat = malloc(sizeof(char *) * 8);
+    char pieseleMele = "pkqrhb*";
+    for(int i = 0; i < 8; i++)
+        newMat[i] = malloc(sizeof(char) * 8);
+
+    newMat = memcpy(newMat, matrix, 64);
+
+    int sgny[4] = {-1,-2,-1,-1};
+    int sgnx[4] = {0,0,-1,1};
+
+    for (int i = 0; i < 2; i++)
+    {
+        if(matrix[myPiece->y + sgny[i]][myPiece->x + sgnx[i]] == '*') {
+            newMat[myPiece->y + sgny[i]][myPiece->x + sgnx[i]] = 'p';
+            newMat[myPiece->y][myPiece->x] = '*';
+            if (isNotInCheck(newMat))
+            {
+                return 1;
+            }
+            newMat[myPiece->y + sgny[i]][myPiece->x + sgnx[i]] = matrix[myPiece->y][myPiece->x];
+            newMat[myPiece->y][myPiece->x] = 'p';
+            
+        }
+    }
+    for (int i = 2; i < 4; i++)
+    {
+        if(strchr(pieseleMele, matrix[myPiece->y + sgny[i]][myPiece->x + sgnx[i]]) == NULL) {
+            newMat[myPiece->y + sgny[i]][myPiece->x + sgnx[i]] = 'p';
+            newMat[myPiece->y][myPiece->x] = '*';
+            if (isNotInCheck(newMat))
+            {
+                return 1;
+            }
+            newMat[myPiece->y + sgny[i]][myPiece->x + sgnx[i]] = matrix[myPiece->y][myPiece->x];
+            newMat[myPiece->y][myPiece->x] = 'p';
+        }
+    }
+
+    return 0;
+}
+
+bool moveRook(unsigned char **matrix, pieceCoordonate *myPiece) {
+    unsigned char **newMat = malloc(sizeof(char *) * 8);
+    char pieseleMele = "pkqrhb";
+    for(int i = 0; i < 8; i++)
+        newMat[i] = malloc(sizeof(char) * 8);
+
+    newMat = memcpy(newMat, matrix, 64);
+
+    int sgny[4] = {-1,1,0,0};
+    int sgnx[4] = {0,0,-1,1};
+    int x,y;
+    for (int i = 0; i < 4; i++)
+    {
+        x = myPiece->x + sgnx[i];
+        y = myPiece->y + sgny[i];
+        while (x >= 0 && x <= 7 && y >= 0 && y <= 7 && matrix[x][y] == '*')
+        {
+            if(strchr(pieseleMele, matrix[y][x]) == NULL) {
+                newMat[y][x] = 'r';
+                newMat[myPiece->y][myPiece->x] = '*';
+
+                if (isNotInCheck(newMat)) {
+                    return 1;
+                }
+                newMat[y][x] = matrix[y][x];
+                newMat[myPiece->y][myPiece->x] = 'r';                
+            }
+            
+            x += sgnx[i];
+            y += sgny[i];
+        }
+    }
+
+    return 0;
+}
+
+bool moveBishop(unsigned char **matrix, pieceCoordonate *myPiece) {
+    unsigned char **newMat = malloc(sizeof(char *) * 8);
+    char pieseleMele = "pkqrhb";
+    for(int i = 0; i < 8; i++)
+        newMat[i] = malloc(sizeof(char) * 8);
+
+    newMat = memcpy(newMat, matrix, 64);
+
+    int sgny[4] = {-1,-1,1,1};
+    int sgnx[4] = {-1,1,-1,1};
+    int x,y;
+    for (int i = 0; i < 4; i++)
+    {
+        x = myPiece->x + sgnx[i];
+        y = myPiece->y + sgny[i];
+        while (x >= 0 && x <= 7 && y >= 0 && y <= 7 && matrix[y][x] == '*')
+        {
+            if(strchr(pieseleMele, matrix[y][x]) == NULL) {
+                newMat[y][x] = 'b';
+                newMat[myPiece->y][myPiece->x] = '*';
+
+                if (isNotInCheck(newMat)) {
+                    return 1;
+                }
+                newMat[y][x] = matrix[y][x];
+                newMat[myPiece->y][myPiece->x] = 'b';                
+            }
+            
+            x += sgnx[i];
+            y += sgny[i];
+        }
+    }
+
+    return 0;
+}
+
+bool moveKnight(unsigned char **matrix, pieceCoordonate *myPiece) {
+    unsigned char **newMat = malloc(sizeof(char *) * 8);
+    char pieseleMele = "pkqrhb";
+    for(int i = 0; i < 8; i++)
+        newMat[i] = malloc(sizeof(char) * 8);
+
+    newMat = memcpy(newMat, matrix, 64);
+
+    int sgny[8] = {-1,-1,1,1,-2,-2,2,2};
+    int sgnx[8] = {-2,2,-2,2,-1,1,-1,1};
+    int x,y;
+    for (int i = 0; i < 8; i++)
+    {
+        x = myPiece->x + sgnx[i];
+        y = myPiece->y + sgny[i];
+        if (x >= 0 && x <= 7 && y >= 0 && y <= 7)
+        {
+            if(strchr(pieseleMele, matrix[y][x]) == NULL) {
+                newMat[y][x] = 'h';
+                newMat[myPiece->y][myPiece->x] = '*';
+
+                if (isNotInCheck(newMat)) {
+                    return 1;
+                }
+                newMat[y][x] = matrix[y][x];
+                newMat[myPiece->y][myPiece->x] = 'h';
+            }
+        }
+    }
+
+    return 0;
+}
+
+bool moveQueen(unsigned char **matrix, pieceCoordonate *myPiece) {
+    unsigned char **newMat = malloc(sizeof(char *) * 8);
+    char pieseleMele = "pkqrhb";
+    for(int i = 0; i < 8; i++)
+        newMat[i] = malloc(sizeof(char) * 8);
+
+    newMat = memcpy(newMat, matrix, 64);
+
+    int sgny[8] = {-1,-1,1,1,-1,1,0,0};
+    int sgnx[8] = {-1,1,-1,1,0,0,-1,1};
+    int x,y;
+    for (int i = 0; i < 8; i++)
+    {
+        x = myPiece->x + sgnx[i];
+        y = myPiece->y + sgny[i];
+        while (x >= 0 && x <= 7 && y >= 0 && y <= 7 && matrix[y][x] == '*')
+        {
+            if(strchr(pieseleMele, matrix[y][x]) == NULL) {
+                newMat[y][x] = 'q';
+                newMat[myPiece->y][myPiece->x] = '*';
+
+                if (isNotInCheck(newMat)) {
+                    return 1;
+                }
+                newMat[y][x] = matrix[y][x];
+                newMat[myPiece->y][myPiece->x] = 'q';                
+            }
+            
+            x += sgnx[i];
+            y += sgny[i];
+        }
+    }
+
+    return 0;
+}
+
+bool moveKing(unsigned char **matrix, pieceCoordonate *myPiece) {
+    unsigned char **newMat = malloc(sizeof(char *) * 8);
+    char pieseleMele = "pkqrhb";
+    for(int i = 0; i < 8; i++)
+        newMat[i] = malloc(sizeof(char) * 8);
+
+    newMat = memcpy(newMat, matrix, 64);
+
+    int sgny[8] = {-1,-1,1,1,-1,1,0,0};
+    int sgnx[8] = {-1,1,-1,1,0,0,-1,1};
+    int x,y;
+    for (int i = 0; i < 8; i++)
+    {
+        x = myPiece->x + sgnx[i];
+        y = myPiece->y + sgny[i];
+        if (x >= 0 && x <= 7 && y >= 0 && y <= 7)
+        {
+            if(strchr(pieseleMele, matrix[y][x]) == NULL) {
+                newMat[y][x] = 'k';
+                newMat[myPiece->y][myPiece->x] = '*';
+
+                if (isNotInCheck(newMat)) {
+                    return 1;
+                }
+                newMat[y][x] = matrix[y][x];
+                newMat[myPiece->y][myPiece->x] = 'k';                
+            }
+        }
+    }
+
+    return 0;
 }
 
 bool isInCheckMate(unsigned char **matrix) {
@@ -16,9 +228,7 @@ bool isInCheckMate(unsigned char **matrix) {
                 myPiece->y = j;
                 myPiece->x = l;
 
-                unsigned char** newMat = movePawn(matrix, myPiece);
-
-                if (isNotInCheck(newMat))
+                if (movePawn(matrix, myPiece))
                 {
                     return 0;
                 }
@@ -30,9 +240,7 @@ bool isInCheckMate(unsigned char **matrix) {
                 myPiece->y = j;
                 myPiece->x = l;
 
-                unsigned char** newMat = moveRook(matrix, myPiece);
-
-                if (isNotInCheck(newMat))
+                if (moveRook(matrix, myPiece))
                 {
                     return 0;
                 }
@@ -44,23 +252,19 @@ bool isInCheckMate(unsigned char **matrix) {
                 myPiece->y = j;
                 myPiece->x = l;
 
-                unsigned char** newMat = movehorse(matrix, myPiece);
-
-                if (isNotInCheck(newMat))
+                if (moveKnight(matrix, myPiece))
                 {
                     return 0;
                 }
                 
             }
-            if (matrix[j][l] == 'h')
+            if (matrix[j][l] == 'b')
             {
-                myPiece->piece = 'h';
+                myPiece->piece = 'b';
                 myPiece->y = j;
                 myPiece->x = l;
 
-                unsigned char** newMat = moveKnight(matrix, myPiece);
-
-                if (isNotInCheck(newMat))
+                if (moveBishop(matrix, myPiece))
                 {
                     return 0;
                 }
@@ -72,9 +276,7 @@ bool isInCheckMate(unsigned char **matrix) {
                 myPiece->y = j;
                 myPiece->x = l;
 
-                unsigned char** newMat = moveQueen(matrix, myPiece);
-
-                if (isNotInCheck(newMat))
+                if (moveQueen(matrix, myPiece))
                 {
                     return 0;
                 }
@@ -86,9 +288,7 @@ bool isInCheckMate(unsigned char **matrix) {
                 myPiece->y = j;
                 myPiece->x = l;
 
-                unsigned char** newMat = moveKing(matrix, myPiece);
-
-                if (isNotInCheck(newMat))
+                if (moveKing(matrix, myPiece))
                 {
                     return 0;
                 }
